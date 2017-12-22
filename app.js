fcef2f7fcf4f8e3f7846b94b2a237834ed728dd4
magik111 <33548156+magik111@users.noreply.github.com>
2017-12-22 14:31:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,19 @@ client.on('ready', () => {
   client.user.setPresence({ game: { name: 'at www.divinerealms.us', type: 0 } });
 });
 
+client.on("message", message => {
+var args = message.content.split(/[ ]+/)
+const embed = new Discord.RichEmbed()
+  .setDescription(`Divine Realms [website](https://www.divinerealms.us), a minecraft ip je DivineRealms.serv.nu`)
+  .setAuthor(`Divine Realms`, `${client.user.displayAvatarURL}`)
+  .setColor([0, 255, 153])
+  .setFooter("Copyright PickledPotato, 2016-2017")
+  .setTimestamp(new Date())
+  .addField("Hvala na strpljenju!", "Vasu prijavu ce pogledati staff clan sto brze moguce." )
+  if(message.guild.id === "237171768693882890" && message.content.startsWith("--report")){
+    message.channel.send({embed});
+    
+        }};
+    });
+
 client.login(process.env.BOT_TOKEN);
