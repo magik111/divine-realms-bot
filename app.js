7b3fb71e4ab1997781922a3ef6e4734c64fade20
magik111 <33548156+magik111@users.noreply.github.com>
2017-12-22 14:14:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,32 @@ const client = new Discord.Client();
 client.on('ready', () => {
   client.user.setPresence({ game: { name: 'at www.divinerealms.us', type: 0 } });
 });
+//Report
+client.on("message", message => {
+var args = message.content.split(/[ ]+/)
+const embed = new Discord.RichEmbed()
+  .setDescription((args.join(" ").substring(8)))
+  .setAuthor(`${message.author.username} prijava:`, `${message.author.displayAvatarURL}`)
+  .setColor([66, 134, 244])
+  .setFooter("© PickledPotato, 2017")
+  .addField("Hvala na strpljenju!", "Vasu prijavu ce pogledati staff clan sto brze moguce." )
+  if(message.guild.id === "237171768693882890" && message.content.startsWith("--report")){
+    if(args.length === 1){
+       message.channel.send("Nedostaje argument. `--report (tekst)` da pravilno postavite prijavu.");
+     } else {
+    client.channels.get("361531026658557963").send({embed});
+    message.delete(setTimeout(function () {
 
+        }, 0));
+        }};
+    });
+//Promo
 client.on("message", message => {
 var args = message.content.split(/[ ]+/)
 const embed = new Discord.RichEmbed()
   .setDescription((args.join(" ").substring(8)))
   .setAuthor(`Prijava od ${message.author.username}:`, `${message.author.displayAvatarURL}`)
-  .setColor([0, 255, 153])
+  .setColor([66, 134, 244])
   .setFooter("© PickledPotato, 2017")
   if(message.guild.id === "237171768693882890" && message.content.startsWith("--promo")){
     if(args.length === 1){
@@ -21,6 +40,16 @@ const embed = new Discord.RichEmbed()
 
         }, 0));
         }};
+    });
+//Ping
+  if(message.content.startsWith(prefix + "ping")) {
+    message.delete(setTimeout(function () {
+
+    }, 10));
+    message.channel.send('[**Beat**] - Pinging...').then(sent => {
+      sent.edit(`[**DivineRealms**] - Pong! Trajalo je ${sent.createdTimestamp - message.createdTimestamp}ms (**${message.author.username}**)`);
+        })
+      }
     });
 
 client.login(process.env.BOT_TOKEN);
