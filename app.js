37b85f9bb05936630ca1d2da480e61c9526a48f4
magik111 <33548156+magik111@users.noreply.github.com>
2017-12-22 13:25:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log("i'm ready bitchez")
-  client.user.setActivity('www.divinerealms.us', { type: 'WATCHING' })
+  console.log("I am ready");
+  client.user.setPresence({ game: { name: 'at www.divinerealms.us', type: 0 } });
+});
+
 });
 client.login(process.env.BOT_TOKEN);
