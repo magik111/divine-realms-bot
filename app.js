77671e394fd9ac5126de19f4a56f9191e9b97b9b
magik111 <33548156+magik111@users.noreply.github.com>
2017-12-22 14:24:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,33 +4,16 @@ const client = new Discord.Client();
 client.on('ready', () => {
   client.user.setPresence({ game: { name: 'at www.divinerealms.us', type: 0 } });
 });
-//Promo
+//Website
 client.on("message", message => {
 var args = message.content.split(/[ ]+/)
 const embed = new Discord.RichEmbed()
-  .addField((args.join(" ").substring(8)))
-  .setAuthor(`Prijava od ${message.author.username}:`, `${message.author.displayAvatarURL}`)
+  .addField(`Website je [Divine Realms]("https://www.divinerealms.us")`)
+  .setAuthor(`${message.author.username} prijava:`, `${message.author.displayAvatarURL}`)
   .setColor([0, 255, 153])
   .setFooter("© PickledPotato, 2017")
-  if(message.guild.id === "237171768693882890" && message.content.startsWith("--promo")){
-    if(args.length === 1){
-       message.channel.send("Nedostaje argument. `--promo (dokaz uploadan na imgur.com ili pomocu Lightshota)` da pravilno postavite prijavu.");
-     } else {
-    client.channels.get("381804087202676736").send({embed});
-    message.delete(setTimeout(function () {
-
-        }, 0));
-        }};
-    });
-//Ping
-  if(message.content.startsWith(prefix + "ping")) {
-    message.delete(setTimeout(function () {
-
-    }, 10));
-    message.channel.send('[**Beat**] - Pinging...').then(sent => {
-      sent.edit(`[**DivineRealms**] - Pong! Trajalo je ${sent.createdTimestamp - message.createdTimestamp}ms (**${message.author.username}**)`);
-        })
-      }
-    });
-
+  .setTimestamp(new Date())
+  if(message.guild.id === "237171768693882890" && message.content.startsWith("--website")){
+ message.channel.send({embed})
+  };
 client.login(process.env.BOT_TOKEN);
