f6a79b1da16b1cf148f42fa3f21eb28cedafb940
magik111 <33548156+magik111@users.noreply.github.com>
2017-12-22 14:27:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,5 @@ const client = new Discord.Client();
 client.on('ready', () => {
   client.user.setPresence({ game: { name: 'at www.divinerealms.us', type: 0 } });
 });
-//Website
-client.on("message", message => {
-var args = message.content.split(/[ ]+/)
-const embed = new Discord.RichEmbed()
-  .addField(`Website je [Divine Realms]("https://www.divinerealms.us")`)
-  .setAuthor(`${message.author.username} prijava:`, `${message.author.displayAvatarURL}`)
-  .setColor([0, 255, 153])
-  .setFooter("© PickledPotato, 2017")
-  .setTimestamp(new Date())
-  if(message.guild.id === "237171768693882890" && message.content.startsWith("--website")){
- message.channel.send({embed})
-  };
+
 client.login(process.env.BOT_TOKEN);
