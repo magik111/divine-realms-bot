a6a0e599807024ca646dbd5c262ab36debc7ee5d
magik111 <33548156+magik111@users.noreply.github.com>
2017-12-22 13:27:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log("I am ready");
   client.user.setPresence({ game: { name: 'at www.divinerealms.us', type: 0 } });
 });
 
